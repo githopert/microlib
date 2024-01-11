1889dd6d84f246c2817bf20e84ed3a74fce80e88
fat-fox-ghost <skypehopert@yandex.ru>
2024-01-11 16:51:21 +0300

    simplification

--- microlib.py
+++ microlib.py
@@ -2,7 +2,7 @@
 
 author: fat_fox
 creation date: 31-Aug-2021
-last modified: 10-Feb-2021
+last modified: 11-Jan-2024
 """
 import argparse
 import json
@@ -10,128 +10,146 @@ import os
 from random import randrange
 
 
-# Variables
-symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
-length = 20
-with open('settings.json', 'r') as r:
-    settings = json.load(r)
+# Globals ------------------------------------------------------------ #
+C_NAME_SYMBOLS = ('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    '1234567890')
+C_NAME_LENGTH = 20
+C_SETTINGS_FILE_NAME = 'microlib_settings.json'
 
 
-# Function implementations
-def check_date(date: str, text: str) -> bool:
-    """ Compares the date in the card with the specified date.
-    """
-    return date == text[:len(date)]
+class MicroLibrary:
+    """ A primitive implementation of a digital zettelkasten. """
 
+    def __init__(self):
+        self.settings = None
 
-def check_number(number: str, text: str) -> bool:
-    """ Compares the card number with the specified number.
-    """
-    return False if len(number) > len(text) else text[:len(number)] == number
+        with open(C_SETTINGS_FILE_NAME, 'r', encoding='utf-8') as r:
+            self.settings = json.load(r)
 
+    # ---------------------------------------------------------------- #
+    # PUBLIC METHODS
+    # ---------------------------------------------------------------- #
+    def find_cards(self, args: argparse.Namespace) -> None:
+        """ Searches for an index card using the specified arguments. """
+        relevant_cards = []
 
-def check_references(reference: str, text: str) -> bool:
-    """ Checks if the specified reference is in the list of references in
-        the index card.
-    """
-    return reference in text.split('/')
+        if (args.date == '' and args.number == '' and args.reference == ''
+            and args.type == '' and args.words == ''):
+            print('No arguments => Nothing to do.')
+            return
 
+        # Searching for cards in the current directory
+        for file in os.listdir(self.settings['cards_dir']):
+            if not os.path.splitext(file)[1] == '.txt':
+                continue
 
-def check_words(words: str, text: str) -> bool:
-    """ Checks if the specified words are in the index card.
-    """
-    return words.lower() in text.lower()
-
-
-def find_card(args: argparse.Namespace) -> None:
-    """ Searches for an index card using the specified arguments.
-    """
-    relevant_cards = []
-
-    # Stop execution if there are no arguments
-    if args.date == '' and args.number == '' and args.reference == '' and\
-        args.words == '':
-        print('No arguments!')
-        return
-    
-    # Searching for cards in the current directory
-    for file in os.listdir(settings['cards_path']):
-        if os.path.splitext(file)[1] == '.txt':
+            card_full_path = os.path.join(
+                self.settings['cards_dir'], file)
             
-            checks = [False] * 4
-            checked = [False] * 4
+            checks = [False] * 5
+            checked = [False] * 5
 
-            with open(os.path.join(settings['cards_path'], file), 'r',
-                                                    encoding='utf-8') as f:
+            with open(card_full_path, 'r', encoding='utf-8') as rf:
                 # Reading the header of the file
-                # Index card number
-                current_number = f.readline().strip('\n') 
-                if args.number.strip() != '':
-                    checks[0] = check_number(args.number.strip(),
-                                             current_number)
-                    checked[0] = True
+                current_number = rf.readline().strip('\n')
+                current_references = rf.readline().strip('\n')
+                current_date = rf.readline().strip('\n')
+                current_type = rf.readline().strip('\n')
+                current_body = " ".join(
+                    [l.strip('\n') for l in rf.readlines()
+                    if l.strip('\n') != '']
+                )
 
-                # References
-                if args.reference.strip() != '':
-                    checks[1] = check_references(args.reference.strip(),
-                                                 f.readline().strip('\n'))
-                    checked[1] = True
-                else:
-                    f.readline()
+            # Reading the header of the file
+            if args.number.strip() != '':
+                checks[0] = self._compare_card_number(
+                    args.number.strip(), current_number)
+                checked[0] = True
 
-                # Date
-                if args.date.strip() != '':
-                    checks[2] = check_date(args.date.strip(),
-                                           f.readline().strip('\n'))
-                    checked[2] = True
-                else:
-                    f.readline()
+            if args.reference.strip() != '':
+                checks[1] = self._compare_card_references(
+                    args.reference.strip(), current_references)
+                checked[1] = True
 
-                # Reading the rest of the file
-                if args.words.strip() != '':
-                    text = " ".join([l.strip('\n') for l in f.readlines()
-                                    if l.strip('\n') != ''])
-                    checks[3] = check_words(args.words, text)
-                    checked[3] = True
+            # Date
+            if args.date.strip() != '':
+                checks[2] = self._compare_card_date(
+                    args.date.strip(), current_date)
+                checked[2] = True
+
+            # Type
+            if args.type.strip() != '':
+                checks[3]= self._compare_card_type(
+                    args.type.strip(), current_type)
+                checked[3] = True
+
+            # Reading the rest of the file
+            if args.words.strip() != '':
+                checks[4] = self._compare_words(args.words, current_body)
+                checked[4] = True
 
             # Accept or reject this card
             if True in checks and checks == checked:
-                relevant_cards.append('{:<15} | {:<22}'.format(
-                                                    current_number, file))
+                relevant_cards.append(
+                    '{:<12} | {:<22}'.format(current_number, file)
+                )
 
-    print_card_list(relevant_cards)
+        self._print_card_list(relevant_cards)
 
+    def generate_card_name(self) -> str:
+        """ Generates a name for the new index card. """
+        new_name = ""
 
-def generate_name() -> str:
-    """ Generates a name for the new index card.
-    """
-    new_name = ""
+        while len(new_name) < C_NAME_LENGTH:
+            new_name += C_NAME_SYMBOLS[randrange(0, len(C_NAME_SYMBOLS)) - 1]
+        
+        return new_name + '.txt'
 
-    while len(new_name) < length:
-        new_name += symbols[randrange(0, len(symbols))-1]
+    # ---------------------------------------------------------------- #
+    # PRIVATE METHODS
+    # ---------------------------------------------------------------- #
+    def _compare_card_date(self, input_date: str, card_date: str) -> bool:
+        """ Compares the date in the string with the specified date. """
+        return input_date == card_date[:len(input_date)]
+
+    def _compare_card_number(self, input_number: str, card_number: str) -> bool:
+        """ Compares the card number with the specified number. """
+        return (
+            False 
+            if len(input_number) > len(card_number)
+            else card_number[:len(input_number)] == input_number
+        )
+
+    def _compare_card_references(self, input_reference: str,
+                                card_references: str) -> bool:
+        """ Checks if the reference is cited in the index card. """
+        return input_reference in card_references.split('/')
     
-    return new_name + '.txt'
+    def _compare_card_type(self, input_type: str, card_type: str):
+        """ Checks the type of the given card. """
+        return input_type == card_type
+
+    def _compare_words(self, input_words: str, card_body: str) -> bool:
+        """ Checks if the specified words are in the index card body. """
+        return input_words.lower() in card_body.lower()
+
+    def _print_card_list(self, card_list: list) -> None:
+        """ Prints the specified list of index cards. """
+        if len(card_list) > 0:
+            print('{:<5} {:<12} | {:<22}'.format('#.', 'card number', 'card file'))
+            print('-' * 40)
+
+            for i, card_file_name in enumerate(sorted(card_list)):
+                print(f'{str(i + 1) + ".":<5} {card_file_name}')
+        else:
+            print('Nothing was found!')
 
 
-def print_card_list(cl: list) -> None:
-    """ Prints the specified list of index cards.
-    """
-    if len(cl) > 0:
-        # Header
-        print('{:<15} | {:<22}'.format('card number', 'card file'))
-        print('-' * 49)
-        # Body
-        for card in sorted(cl): print(card)
-    else:
-        print('Nothing was found!')
-
-
-# Main
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='`microlib v0.1` - searching'
         ' for an index card in the specified directory. To select the '
-        'directory, modify the file `settings.json`')
+        'directory, modify the file `microlib_settings.json`')
     
     # Search arguments
     parser.add_argument('-n', dest='number', default='', type=str,
@@ -141,30 +159,26 @@ if __name__ == '__main__':
     parser.add_argument('-d', dest='date', default='', type=str,
                         help='a creation date in the YYYYMMDD-format. '
                         'Example: 20210831')
+    parser.add_argument('-t', dest='type', default='', type=str,
+                        help='n - for notes, a - for auxiliary cards, ' +
+                        's - for summaries.')
     parser.add_argument('-w', dest='words', default='', type=str,
                         help='keywords. Example: INDEX')
     # Card manipulation arguments
     parser.add_argument('--generate', dest='generate',
                         action='store_true', help='generates a new card')
-    parser.add_argument('--open', dest='open', default='', type=str,
-                        help='opens the card specified by name. '
-                        'Example: ./microlib -o <filename.txt>')
-    parser.add_argument('--remove', dest='remove', default='', type=str,
-                        help='removes the index card '
-                        'specified by name')
  
     args = parser.parse_args()
+    microlib = MicroLibrary()
+    card_dir = microlib.settings['cards_dir']
+    editor = microlib.settings['editor']
+
+    if not os.path.exists(card_dir):
+        os.mkdirs(card_dir)
 
     if args.generate:
-        new_name = generate_name()
-        print("Creating the file: " +
-                        os.path.join(settings['cards_path'], new_name))
-        os.system(settings['editor'] + ' ' +
-                        os.path.join(settings['cards_path'], new_name))
-    elif args.open != '':
-        os.system(settings['editor'] + ' ' +
-                        os.path.join(settings['cards_path'], args.open))
-    elif args.remove != '':
-        os.remove(os.path.join(settings['cards_path'], args.remove))
+        new_name = microlib.generate_card_name()
+        print("Creating the file: " + os.path.join(card_dir, new_name))
+        os.system(editor + ' ' + os.path.join(card_dir, new_name))
     else:
-        find_card(args)
+        microlib.find_cards(args)
